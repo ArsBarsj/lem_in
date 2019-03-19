df2ee571753010221d7b56007943a9e41b9596d7
Falmer shadowmaster Kuhn <fkuhn@e4r8p9.21-school.ru>
2019-03-19 21:26:26 +0300

    bug fix

--- bfs_alg_utils.c
+++ bfs_alg_utils.c
@@ -6,7 +6,7 @@
 /*   By: fkuhn <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 20:48:40 by artemiy           #+#    #+#             */
-/*   Updated: 2019/03/19 18:44:20 by fkuhn            ###   ########.fr       */
+/*   Updated: 2019/03/19 21:25:17 by fkuhn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,26 @@ int		reset_best(t_path ***best, int *nm, t_path ***tmp, t_path **alt)
 	free(*best);
 	*best = alt;
 	nm[2] = count_paths(*best) - 1;
-	return ((*best)[nm[0]]->len - 1);
+	return (1);
 }
 
 void	go_next(t_graph *g, t_path **b, t_config *cfg, int *nm)
 {
 	graph_link_add(g, b[nm[0]]->path[nm[1] - 1]->id,\
 					b[nm[0]]->path[nm[1]]->id, 2);
-	if (nm[1] > 1)
-		nm[1]--;
+	if (nm[1] < b[nm[0]]->len - 1)
+	{
+		nm[1]++;
+		path_close_all(g, b, cfg->start_id, cfg->end_id);
+		path_restore_links(b[nm[0]], g);
+	}
 	else
 	{
 		nm[0]++;
 		path_close_all(g, b, cfg->start_id, cfg->end_id);
 		path_restore_links(b[nm[0]], g);
 		if (b[nm[0]])
-			nm[1] = b[nm[0]]->len - 1;
+			nm[1] = 1;
 		else
 			nm[1] = 0;
 	}
@@ -52,8 +56,9 @@ void	skip_nodes(t_graph *g, t_path **b, int *nm)
 
 t_path	**find_new_path(t_graph *g, t_path **b, t_config *cfg, int *nm)
 {
-	while (graph_links_num(g, b[nm[0]]->path[nm[1] - 1]->id) < 3 && nm[1] - 1)
-		nm[1]--;
+	while (graph_links_num(g, b[nm[0]]->path[nm[1] - 1]->id) < 3 &&
+			nm[1] + 1 < b[nm[0]]->len)
+		nm[1]++;
 	graph_link_del(&g, b[nm[0]]->path[nm[1] - 1]->id,\
 					b[nm[0]]->path[nm[1]]->id, 2);
 	return (bfs_ways2(cfg->start_id, cfg->end_id, g));
@@ -64,4 +69,5 @@ void	init_search(t_graph *g, t_path **b, t_config *cfg, int *nm)
 	nm[2] = count_paths(b) - 1;
 	nm[0] = -1;
 	nm[1] = update_state2(g, b, cfg, &(nm[0]));
+	nm[1] = 1;
 }
